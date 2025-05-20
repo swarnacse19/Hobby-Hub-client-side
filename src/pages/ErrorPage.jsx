22f0b615d6554672dda8d7066b2f985b0ead217e
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-05-20 22:10:42 +0600

    create errorpage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -9,8 +9,8 @@ function ErrorPage() {
     <>
     <div className='flex justify-center items-center min-h-screen'>
       <div className='text-center'>
-        <img className='mx-auto' src="https://i.ibb.co.com/qYw36jgD/error.png" alt="" />
-      <h1 className='font-bold text-2xl text-red-500'>404 - Page not found</h1>
+        <img className='mx-auto' src="https://i.ibb.co/9HTmgvC1/404.jpg" alt="" />
+      <h1 className='font-medium text-xl text-red-500'>Page not found</h1>
       <button
             onClick={() => navigate("/")}
             className="bg-green-500 text-white px-6 py-2 rounded-full mt-4"
