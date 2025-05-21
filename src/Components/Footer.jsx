6ea8085b311806e20a8d986ee51722c37d41ec57
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-05-21 16:39:17 +0600

    added footer section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React from "react";
+import { FaFacebookF, FaLinkedin, FaGithub } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
+import { Link } from "react-router";
+import hobbyHub from "../assets/hobbyHub.png";
 
 function Footer() {
+  const links = (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/groups">All Groups</Link>
+      <Link to="/createGroup">Create Group</Link>
+      <Link to="/myGroups ">My Groups</Link>
+    </>
+  );
+
   return (
-    <div>
-      <h1>this is footer</h1>
+    <div className="h-[423px] py-25 px-20 flex justify-center items-center bg-pink-400 text-white">
+      <div>
+        <div className="flex justify-center mb-5 items-center gap-2">
+          <img src={hobbyHub} className="w-10" alt="" />
+                    <h2 className="text-2xl font-semibold">Hobby<span className="text-pink-700">Hub</span></h2>
+        </div>
+        <div className="flex gap-5 list-none">{links}</div>
+        <div className="flex gap-5 justify-center mt-4">
+          <a href="https://facebook.com" target="_blank">
+            <FaFacebookF
+              className= "rounded-2xl pt-1 bg-white"
+              size={30} color="blue"
+            />
+          </a>
+          <a href="https://twitter.com" target="_blank">
+            <BsTwitterX size={30} color="white" />
+          </a>
+          <a href="https://linkedin.com" target="_blank">
+            <FaLinkedin size={35} color="white" />
+          </a>
+          <a href="https://github.com" target="_blank">
+            <FaGithub size={35} color="white" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
