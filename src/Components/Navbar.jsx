b7380daec33067bbd46dff8afe49b49cb9d821b6
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-05-22 02:06:51 +0600

    added user profile icon and display name

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,59 +1,59 @@
 import React, { use, useEffect, useState } from "react";
 import { Link, NavLink, useLocation } from "react-router";
-// import { AuthContext } from "../provider/AuthProvider";
+import { AuthContext } from "../provider/AuthProvider";
 import { ToastContainer, toast } from "react-toastify";
 import hobbyHub from "../assets/hobbyHub.png";
-import { MdDarkMode, MdOutlineLightMode } from 'react-icons/md';
+import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
 
 function Navbar() {
   const location = useLocation();
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
-    const saved = localStorage.getItem('home-theme') || 'light';
+    const saved = localStorage.getItem("home-theme") || "light";
     setTheme(saved);
 
-    if (location.pathname === '/') {
+    if (location.pathname === "/") {
       document.body.className = saved;
     } else {
-      document.body.className = '';
+      document.body.className = "";
     }
 
     return () => {
-      document.body.className = '';
+      document.body.className = "";
     };
   }, [location.pathname]);
 
   const toggleTheme = () => {
-  const newTheme = theme === 'light' ? 'dark' : 'light';
-  setTheme(newTheme);
-  localStorage.setItem('home-theme', newTheme);
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("home-theme", newTheme);
 
-  if (location.pathname === '/') {
-    document.body.className = newTheme;
-  }
-};
-
+    if (location.pathname === "/") {
+      document.body.className = newTheme;
+    }
+  };
 
   const links = (
     <>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/groups">All Groups</NavLink>
-        <NavLink to="/createGroup">Create Group</NavLink>
-        <NavLink to="/myGroups ">My Groups</NavLink>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/groups">All Groups</NavLink>
+      <NavLink to="/createGroup">Create Group</NavLink>
+      <NavLink to="/myGroups ">My Groups</NavLink>
     </>
   );
-//   const { user, logOut } = use(AuthContext);
-//   const handleLogOut = () => {
-//     logOut()
-//       .then(() => {
-//         toast.success("Logout Successfully!");
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   };
-//   console.log(user);
+  const { user, logOut } = use(AuthContext);
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {
+        toast.success("Logout Successfully!");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  console.log(user);
+
   return (
     <div className="navbar">
       <div className="navbar-start">
@@ -84,39 +84,61 @@ function Navbar() {
         </div>
         <div className="flex items-center gap-2">
           <img src={hobbyHub} className="w-10" alt="" />
-          <h2 className="text-2xl font-semibold">Hobby<span className="text-pink-700">Hub</span></h2>
+          <h2 className="text-2xl font-semibold">
+            Hobby<span className="text-pink-700">Hub</span>
+          </h2>
         </div>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 gap-7 text-xl font-light">{links}</ul>
+        <ul className="menu menu-horizontal px-1 gap-7 text-xl font-light">
+          {links}
+        </ul>
       </div>
       <div className="navbar-end">
-      <div className="flex items-center gap-3">
-        {/* <img
-          className="w-12 rounded-full"
-          src={`${user ? user.photoURL : userIcon}`}
-          alt="" title={user ? user.displayName : "Guest"}
+        <div className="flex items-center gap-3">
 
-        />
-        {user ? (
-          <button
-          onClick={handleLogOut}
-          className="btn bg-blue-600 text-white font-semibold"
-        >
-          Logout
-        </button>
-        ) : (
-          <Link to="/login" className="btn bg-blue-600 text-white font-semibold">
-            Login
-          </Link>
-        )} */} <Link to="/login" className="btn bg-white text-pink-700 border-2 border-pink-700 font-semibold hover:bg-pink-200">Login</Link>
-        <Link to="/register" className="btn bg-white text-green-700 border-2 border-green-700 font-semibold hover:bg-green-200">Register</Link>
-        <div onClick={toggleTheme}>    
-          {theme === 'light' ? <MdDarkMode size={30}/> : <MdOutlineLightMode size={30}/>}
+          {user && (
+            <img
+              className="w-12 rounded-full"
+              src={user.photoURL}
+              alt=""
+              title={user.displayName}
+            />
+          )}
+
+          {user ? (
+            <button
+              onClick={handleLogOut}
+              className="btn bg-pink-700 text-white font-semibold"
+            >
+              Logout
+            </button>
+          ) : (
+            <div>
+              {/* <Link
+                to="/login"
+                className="btn bg-white text-pink-700 border-2 border-pink-700 font-semibold hover:bg-pink-200"
+              >
+                Login
+              </Link> */}
+              <Link
+                to="/register"
+                className="btn bg-white text-green-700 border-2 border-green-700 font-semibold hover:bg-green-200"
+              >
+                Register
+              </Link>
+            </div>
+          )}
+          <div onClick={toggleTheme}>
+            {theme === "light" ? (
+              <MdDarkMode size={30} />
+            ) : (
+              <MdOutlineLightMode size={30} />
+            )}
+          </div>
         </div>
       </div>
-      </div>
-      <ToastContainer position="top-right" autoClose={3000} /> 
+      <ToastContainer position="top-right" autoClose={3000} />
     </div>
   );
 }
