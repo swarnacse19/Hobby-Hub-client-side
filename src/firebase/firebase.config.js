b21220b5c1cd6989ad2368df77111ba7fdb91def
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-05-22 01:15:03 +0600

    hide firebase credentials

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjC4PPxrqt6_EPoHLsAlMc2qGaizzrznw",
-  authDomain: "hobby-hub-auth-a7450.firebaseapp.com",
-  projectId: "hobby-hub-auth-a7450",
-  storageBucket: "hobby-hub-auth-a7450.firebasestorage.app",
-  messagingSenderId: "603324155294",
-  appId: "1:603324155294:web:ae683e9eb7a380747a028f"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
