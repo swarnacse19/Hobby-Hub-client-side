44fa46ceef087d05f0d4a2c598c90aaf522c39cb
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-05-21 16:28:32 +0600

    added lottie-react animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import HubSlider from '../Components/HubSlider';
+import Lottie from 'lottie-react';
+import animationData from './Welcome.json';
 
 function Home() {
   return (
     <div>
       <HubSlider></HubSlider>
+      <div className='mx-auto' style={{ width: 800, height: 300 }}>
+        <Lottie animationData={animationData} loop={true}/>
+        <p className='text-center font-medium text-green-700'>Explore hobby groups around you and get connected!</p>
+      </div>
     </div>
   );
 }
